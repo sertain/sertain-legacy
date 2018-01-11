f0f6942fa0711c0118a43f13fa74ac50942b73c2
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-11 09:04:39 -0800

    Add Victor motor

--- src/main/kotlin/org/sertain/hardware/Motors.kt
+++ src/main/kotlin/org/sertain/hardware/Motors.kt
@@ -1,7 +1,10 @@
 package org.sertain.hardware
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX
 
 typealias Talon = WPI_TalonSRX
+typealias Victor = WPI_VictorSPX
 
 operator fun Talon.plus (other: Talon) = apply { other.follow(this) }
+operator fun Victor.plus (other: Victor) = apply { other.follow(this) }
