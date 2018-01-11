2ac0c63ee7e31e64c00b94247b60366b7d996273
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-11 09:10:28 -0800

    Add motor stop method

--- src/main/kotlin/org/sertain/hardware/Motors.kt
+++ src/main/kotlin/org/sertain/hardware/Motors.kt
@@ -8,3 +8,6 @@ typealias Victor = WPI_VictorSPX
 
 operator fun Talon.plus(other: Talon) = apply { other.follow(this) }
 operator fun Victor.plus(other: Victor) = apply { other.follow(this) }
+
+fun Talon.stop() = apply { this.stopMotor() }
+fun Victor.stop() = apply { this.stopMotor() }
