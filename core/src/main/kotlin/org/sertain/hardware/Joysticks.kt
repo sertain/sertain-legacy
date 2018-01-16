0dc250d4362e2abca4e6d2d6ae26256d06a53ea8
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-16 13:29:08 -0800

    Allow lambdas in joystick whenActive and whileActive

--- core/src/main/kotlin/org/sertain/hardware/Joysticks.kt
+++ core/src/main/kotlin/org/sertain/hardware/Joysticks.kt
@@ -12,3 +12,31 @@ public fun Joystick.whenActive(buttonNumber: Int, command: Command) {
 public fun Joystick.whileActive(buttonNumber: Int, command: Command) {
     JoystickButton(this, buttonNumber).whileActive(command.mirror)
 }
+
+public fun Joystick.whenActive(buttonNumber: Int, block: () -> Unit) {
+    JoystickButton(this, buttonNumber).whenActive(
+        object : edu.wpi.first.wpilibj.command.Command() {
+            override fun execute() {
+                block()
+            }
+
+            override fun isFinished(): Boolean {
+                return true
+            }
+        }
+    )
+}
+
+public fun Joystick.whileActive(buttonNumber: Int, block: () -> Unit) {
+    JoystickButton(this, buttonNumber).whileActive(
+        object : edu.wpi.first.wpilibj.command.Command() {
+            override fun execute() {
+                block()
+            }
+
+            override fun isFinished(): Boolean {
+                return true
+            }
+        }
+    )
+}
