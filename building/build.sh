4b984c4f9b216b2519dc3feb73373854da6674e5
Alex Saveau <saveau.alexandre@gmail.com>
2018-01-18 14:26:01 -0800

    Move docs to separate repo
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

---
+++ building/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = "master" ]; then
+  ./gradlew clean build dokka
+
+  # Upload docs
+  cd ..
+  git clone --branch=master "https://SUPERCILEX:${GIT_LOGIN}@github.com/sertain/javadocs.git" javadocs &> /dev/null
+  git config --global user.email "saveau.alexandre@gmail.com"
+  git config --global user.name "Alex Saveau"
+
+  cp sertain/core/build/javadocs/** -r javadocs
+  cd javadocs
+
+  git add .
+  git commit -a -m "Update docs from https://github.com/sertain/sertain/compare/${TRAVIS_COMMIT_RANGE}"
+  git push -u origin master &> /dev/null
+else
+  ./gradlew clean assembleDebug check
+fi
