38da64671a18a66d154542787a1d0745c9c358fc
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-19 09:23:47 -0800

    Add subsystem class
    Closes https://github.com/sertain/sertain/issues/28

---
+++ core/src/main/kotlin/org/sertain/command/Subsystem.kt
@@ -0,0 +1,11 @@
+@file:Suppress("unused", "RedundantVisibilityModifier")
+package org.sertain.command
+
+import org.sertain.RobotLifecycle
+
+private typealias WpiLibSubsystem = edu.wpi.first.wpilibj.command.Subsystem
+
+/**
+ * @see edu.wpi.first.wpilibj.command.Subsystem
+ */
+public abstract class Subsystem : WpiLibSubsystem(), RobotLifecycle
