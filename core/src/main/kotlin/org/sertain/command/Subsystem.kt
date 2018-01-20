11c13e094f40f5d696685e8a968fb40ada13dd77
Alex Saveau <saveau.alexandre@gmail.com>
2018-01-19 16:11:51 -0800

    Fix possible NPEs
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- core/src/main/kotlin/org/sertain/command/Subsystem.kt
+++ core/src/main/kotlin/org/sertain/command/Subsystem.kt
@@ -1,6 +1,7 @@
 @file:Suppress("unused", "RedundantVisibilityModifier")
 package org.sertain.command
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder
 import org.sertain.RobotLifecycle
 
 private typealias WpiLibSubsystem = edu.wpi.first.wpilibj.command.Subsystem
@@ -9,7 +10,8 @@ private typealias WpiLibSubsystem = edu.wpi.first.wpilibj.command.Subsystem
  * @see edu.wpi.first.wpilibj.command.Subsystem
  */
 public abstract class Subsystem : WpiLibSubsystem(), RobotLifecycle {
-    init {
+    override fun initSendable(builder: SendableBuilder?) {
+        super.initSendable(builder)
         RobotLifecycle.addListener(this)
     }
 }
