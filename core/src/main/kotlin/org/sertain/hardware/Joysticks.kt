86dfe41d1041cf41605dc206fb003696af14a714
Alex Saveau <saveau.alexandre@gmail.com>
2018-01-22 16:39:42 -0800

    Update Joysticks.kt

--- core/src/main/kotlin/org/sertain/hardware/Joysticks.kt
+++ core/src/main/kotlin/org/sertain/hardware/Joysticks.kt
@@ -1,52 +1,28 @@
 @file:Suppress("unused", "RedundantVisibilityModifier")
-
 package org.sertain.hardware
 
-import edu.wpi.first.wpilibj.Joystick
-import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.buttons.JoystickButton
 import org.sertain.command.Command
 
-public fun Joystick.whenActive(buttonNumber: Int, command: Command) {
+public fun GenericHID.whenActive(buttonNumber: Int, command: Command) {
     JoystickButton(this, buttonNumber).whenActive(command.mirror)
 }
 
-public fun Joystick.whileActive(buttonNumber: Int, command: Command) {
+public fun GenericHID.whileActive(buttonNumber: Int, command: Command) {
     JoystickButton(this, buttonNumber).whileActive(command.mirror)
 }
 
-public inline fun Joystick.whenActive(
+public inline fun GenericHID.whenActive(
         buttonNumber: Int,
         crossinline block: () -> Boolean
 ) = whenActive(buttonNumber, object : Command() {
     override fun execute() = block()
 })
 
-public inline fun Joystick.whileActive(
+public inline fun GenericHID.whileActive(
         buttonNumber: Int,
         crossinline block: () -> Boolean
 ) = whileActive(buttonNumber, object : Command() {
-    override fun execute() = block()
-})
-
-public fun XboxController.whenActive(buttonNumber: Int, command: Command) {
-    this.whenActive(buttonNumber, command)
-}
-
-public fun XboxController.whileActive(buttonNumber: Int, command: Command) {
-    this.whenActive(buttonNumber, command)
-}
-
-public inline fun XboxController.whenActive(
-        buttonNumber: Int,
-        crossinline block: () -> Boolean
-) = whenActive(buttonNumber, object : Command() {
-    override fun execute() = block()
-})
-
-public inline fun XboxController.whileActive(
-        buttonNumber: Int,
-        crossinline block: () -> Boolean
-) = whenActive(buttonNumber, object : Command() {
     override fun execute() = block()
 })
