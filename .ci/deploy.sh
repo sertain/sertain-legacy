57c3f0a2ee38f9b113727172cfe1ad4e78fc8d2c
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-25 09:31:08 -0800

    Skip deploy on timestamp updates

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,7 +2,7 @@
 set -eo pipefail # Exit with nonzero exit code if anything fails
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "doc-deploys" ]; then
     echo "Skipping deploy."
     exit 0
 fi
@@ -11,7 +11,7 @@ fi
 
 # Upload docs
 cd ..
-git clone --branch=master "https://SUPERCILEX:${GIT_LOGIN}@github.com/sertain/javadocs.git" javadocs &> /dev/null
+git clone --branch=master "https://SUPERCILEX:${GIT_LOGIN}@github.com/sertain/javadocs-tests.git" javadocs &> /dev/null
 
 git config --global user.name "Travis CI"
 git config --global user.email "social@sert2521.org"
@@ -19,11 +19,21 @@ git config --global user.email "social@sert2521.org"
 cp -r sertain/core/build/javadocs/** javadocs
 cd javadocs
 
-# If there are no real changes to the compiled out
-# e.g. this is a README update, then just bail.
+# If there are no real changes to the compiled out (e.g. this is a README update), then just bail.
 if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
+fi
+
+# If the only changes are timestamp updates, then bail as well. This can be determined by checking
+# if the number of additions, deletions, and changed files all match up.
+FILES=$(git log -1 --shortstat | awk '/^ [0-9]/ { f += $1; i += $4; d += $6 } END { printf(f) }')
+INSERT=$(git log -1 --shortstat | awk '/^ [0-9]/ { f += $1; i += $4; d += $6 } END { printf(i) }')
+DELETE=$(git log -1 --shortstat | awk '/^ [0-9]/ { f += $1; i += $4; d += $6 } END { printf(d) }')
+
+if [ "$FILES" = "$INSERT" ] && [ "$INSERT" = "$DELETE" ]; then
+   echo "Only timestamp updates; exiting."
+   exit 0
 fi
 
 git add .
