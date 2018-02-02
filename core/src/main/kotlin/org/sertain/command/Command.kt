dc20790e3d8d39b3cede5c67d78026ad22e277a2
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-01 16:33:09 -0800

    Add command group support
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- core/src/main/kotlin/org/sertain/command/Command.kt
+++ core/src/main/kotlin/org/sertain/command/Command.kt
@@ -1,10 +1,39 @@
 @file:Suppress("unused", "RedundantVisibilityModifier")
 package org.sertain.command
 
+import edu.wpi.first.wpilibj.command.CommandGroup
 import edu.wpi.first.wpilibj.command.Subsystem
 import java.util.concurrent.TimeUnit
 
 private typealias WpiLibCommand = edu.wpi.first.wpilibj.command.Command
+
+/** @see CommandGroup.addSequential */
+public infix fun Command.then(command: Command) =
+        CommandGroup().addSequential(this).addSequential(command)
+
+/** @see CommandGroup.addParallel */
+public infix fun Command.and(command: Command) =
+        CommandGroup().addParallel(this).addParallel(command)
+
+/** @see CommandGroup.addSequential */
+public infix fun CommandGroup.then(command: Command) = addSequential(command)
+
+/** @see CommandGroup.addParallel */
+public infix fun CommandGroup.and(command: Command) = addParallel(command)
+
+/** @see CommandGroup.addSequential */
+@JvmOverloads
+public fun CommandGroup.addSequential(command: Command, timeout: Double = 0.0): CommandGroup {
+    addSequential(command.mirror, timeout)
+    return this
+}
+
+/** @see CommandGroup.addParallel */
+@JvmOverloads
+public fun CommandGroup.addParallel(command: Command, timeout: Double = 0.0): CommandGroup {
+    addParallel(command.mirror, timeout)
+    return this
+}
 
 /** @see edu.wpi.first.wpilibj.command.Command */
 public abstract class Command @JvmOverloads constructor(
