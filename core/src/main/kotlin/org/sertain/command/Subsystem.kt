61f6c1897e45a1950083d37cfad4fcd2fffa68dd
Aidan Heller <asfheller@gmail.com>
2018-08-26 11:31:00 -0700

    Add ability to use subsystems as robot class constructor arguments (#53)

--- core/src/main/kotlin/org/sertain/command/Subsystem.kt
+++ core/src/main/kotlin/org/sertain/command/Subsystem.kt
@@ -2,7 +2,6 @@
 @file:JvmName("SubsystemUtils")
 package org.sertain.command
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder
 import org.sertain.RobotLifecycle
 import edu.wpi.first.wpilibj.command.Subsystem as WpiLibSubsystem
 
@@ -10,11 +9,6 @@ import edu.wpi.first.wpilibj.command.Subsystem as WpiLibSubsystem
 public abstract class Subsystem : WpiLibSubsystem(), RobotLifecycle {
     /** @see edu.wpi.first.wpilibj.command.Subsystem.setDefaultCommand */
     open val defaultCommand: CommandBase? = null
-
-    override fun initSendable(builder: SendableBuilder?) {
-        super.initSendable(builder)
-        RobotLifecycle.addListener(this)
-    }
 
     override fun initDefaultCommand() = setDefaultCommand(defaultCommand?.mirror)
 }
